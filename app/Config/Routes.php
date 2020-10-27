8a862b2259434b5d3274291e3f5bc0c374c86bb0
julianacristina.bertobuzanello <julianacristina.bertobuzanello@student.kuleuven.be>
2020-10-27 13:01:26 +0100

    changed routing

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,11 +16,15 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('Newcontroller');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
 $routes->setAutoRoute(true);
+$routes->add('home',   'Newcontroller::home');
+$routes->add('about',  'Newcontroller::about');
+$routes->add('contact',  'Newcontroller::contact');
+$routes->add('vue',  'Newcontroller::vue');
 
 /**
  * --------------------------------------------------------------------
@@ -30,8 +34,7 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
-
+$routes->get('/', 'Newcontroller::home');
 /**
  * --------------------------------------------------------------------
  * Additional Routing
