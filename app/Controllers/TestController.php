eb0981b29734ed198025d25b9131c3579a0536fa
marnix.lijnen <marnix.lijnen@student.kuleuven.be>
2020-10-30 11:40:34 +0100

    changed routing

---
+++ app/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+class TestController extends BaseController
+{
+    public function index(){
+        echo 'hello world';
+    }
+
+    public function  comment(){
+        echo 'comment';
+    }
+}
