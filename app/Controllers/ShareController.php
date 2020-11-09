4c663105c926b28f23614cd37082f6386f18a612
Cemal Yetismis <cemal.yetismis@student.kuleuven.be>
2020-11-09 13:40:46 +0100

    Json php

--- app/Controllers/ShareController.php
+++ app/Controllers/ShareController.php
@@ -2,6 +2,9 @@
 
 
 namespace App\Controllers;
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
 
 
 use App\Models\Share_model;
@@ -24,4 +27,13 @@ class ShareController extends BaseController
         echo json_encode($data);
     }
 
+    public function save(){
+
+        $json = $this->request->getJSON();
+        $data = [
+            'title' => $json->my_title,
+            'date' => $json->my_current_date
+        ];
+        $this->share_model->insert($data);
+    }
 }
