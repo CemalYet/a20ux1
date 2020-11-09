70660aff13697a88dd8b106cb42cfb7dcc5ef6b5
helena.majoor <helena.majoor@student.kuleuven.be>
2020-11-09 19:52:13 +0100

    First try on getting things from db

--- app/Models/Discovery_model.php
+++ app/Models/Discovery_model.php
@@ -40,4 +40,12 @@ class Discovery_model extends Model{
         return $query->getResult();
     }
 
+    public function get_disco_info() {
+        $session = \Config\Services::session();
+        $session->get('ses_data');
+        $query_text = 'SELECT title FROM a20ux1.DiscoveryTable WHERE discoveryId = 1';
+        $query = $this->db->query($query_text);
+        return $query->getResult();
+    }
+
 }
