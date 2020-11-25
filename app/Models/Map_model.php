19f5b0c1ab2c0e90cdaf41416eb285feab3c67e6
seppe.fleerackers <seppe.fleerackers@student.kuleuven.be>
2020-11-25 16:37:20 +0100

    again updated queries

--- app/Models/Map_model.php
+++ app/Models/Map_model.php
@@ -6,27 +6,30 @@ use CodeIgniter\Model;
 
 class Map_model extends Model
 {
-    
+    private $userId;
 
     public function __construct()
     {
         $this->db = \Config\Database::connect();
+        $session = \Config\Services::session()->get('ses_data');
+        $this->userId = $session['userId'];
+        $this->userId = 17;
     }
 
     public function get_my_discoveries() {
-        $query_text = 'SELECT a20ux1.DiscoveryTable.discoveryId, a20ux1.DiscoveryTable.takenDate, a20ux1.DiscoveryTable.Longitude, a20ux1.DiscoveryTable.Latitude, a20ux1.DiscoveryTable.title, a20ux1.DiscoveryTable.location, a20ux1.UserTable.userName FROM a20ux1.DiscoveryTable INNER JOIN a20ux1.UserTable ON a20ux1.DiscoveryTable.userIdFk = a20ux1.UserTable.userId WHERE a20ux1.UserTable.userId=17 AND a20ux1.DiscoveryTable.Longitude AND a20ux1.DiscoveryTable.Latitude;';
+        $query_text = "SELECT a20ux1.DiscoveryTable.discoveryId, a20ux1.DiscoveryTable.takenDate, a20ux1.DiscoveryTable.Longitude, a20ux1.DiscoveryTable.Latitude, a20ux1.DiscoveryTable.title, a20ux1.DiscoveryTable.location, a20ux1.UserTable.userName FROM a20ux1.DiscoveryTable INNER JOIN a20ux1.UserTable ON a20ux1.DiscoveryTable.userIdFk = a20ux1.UserTable.userId WHERE a20ux1.UserTable.userId= '{$this->userId}' AND a20ux1.DiscoveryTable.Longitude AND a20ux1.DiscoveryTable.Latitude;";
         $query = $this->db->query($query_text);
         return $query->getResult();
     }
 
     public function get_friend_discoveries() {
-        $query_text = 'SELECT a20ux1.DiscoveryTable.discoveryId, a20ux1.DiscoveryTable.takenDate, a20ux1.DiscoveryTable.Longitude, a20ux1.DiscoveryTable.Latitude, a20ux1.DiscoveryTable.title, a20ux1.DiscoveryTable.location, a20ux1.UserTable.userName FROM a20ux1.DiscoveryTable INNER JOIN a20ux1.UserTable ON a20ux1.DiscoveryTable.userIdFk = a20ux1.UserTable.userId WHERE a20ux1.DiscoveryTable.userIdFk IN (SELECT DISTINCT sender FROM a20ux1.FriendsTable WHERE receiver=17 AND state=1 UNION SELECT DISTINCT receiver FROM a20ux1.FriendsTable WHERE sender=17 AND state=1) AND a20ux1.DiscoveryTable.Longitude AND a20ux1.DiscoveryTable.Latitude;';
+        $query_text = "SELECT a20ux1.DiscoveryTable.discoveryId, a20ux1.DiscoveryTable.takenDate, a20ux1.DiscoveryTable.Longitude, a20ux1.DiscoveryTable.Latitude, a20ux1.DiscoveryTable.title, a20ux1.DiscoveryTable.location, a20ux1.UserTable.userName FROM a20ux1.DiscoveryTable INNER JOIN a20ux1.UserTable ON a20ux1.DiscoveryTable.userIdFk = a20ux1.UserTable.userId WHERE a20ux1.DiscoveryTable.userIdFk IN (SELECT DISTINCT sender FROM a20ux1.FriendsTable WHERE receiver='{$this->userId}' AND state=1 UNION SELECT DISTINCT receiver FROM a20ux1.FriendsTable WHERE sender='{$this->userId}' AND state=1) AND a20ux1.DiscoveryTable.Longitude AND a20ux1.DiscoveryTable.Latitude;";
         $query = $this->db->query($query_text);
         return $query->getResult();
     }
 
     public function get_popular_discoveries() {
-        $query_text = 'SELECT a20ux1.DiscoveryTable.discoveryId, a20ux1.DiscoveryTable.takenDate, a20ux1.DiscoveryTable.Longitude, a20ux1.DiscoveryTable.Latitude, a20ux1.DiscoveryTable.title, a20ux1.DiscoveryTable.location, a20ux1.UserTable.userName FROM a20ux1.DiscoveryTable INNER JOIN a20ux1.UserTable ON a20ux1.DiscoveryTable.userIdFk = a20ux1.UserTable.userId WHERE discoveryId IN (SELECT discoveryIdFk FROM a20ux1.LikedTable GROUP BY discoveryIdFk HAVING count(*)>1) AND DATE_ADD(takenDate, INTERVAL 30 DAY) >= current_timestamp() AND a20ux1.UserTable.useLocation=1 AND a20ux1.DiscoveryTable.Longitude AND a20ux1.DiscoveryTable.Latitude;';
+        $query_text = "SELECT a20ux1.DiscoveryTable.discoveryId, a20ux1.DiscoveryTable.takenDate, a20ux1.DiscoveryTable.Longitude, a20ux1.DiscoveryTable.Latitude, a20ux1.DiscoveryTable.title, a20ux1.DiscoveryTable.location, a20ux1.UserTable.userName FROM a20ux1.DiscoveryTable INNER JOIN a20ux1.UserTable ON a20ux1.DiscoveryTable.userIdFk = a20ux1.UserTable.userId WHERE discoveryId IN (SELECT discoveryIdFk FROM a20ux1.LikedTable GROUP BY discoveryIdFk HAVING count(*)>1) AND DATE_ADD(takenDate, INTERVAL 30 DAY) >= current_timestamp() AND a20ux1.UserTable.useLocation=1 AND a20ux1.DiscoveryTable.Longitude AND a20ux1.DiscoveryTable.Latitude;";
         $query = $this->db->query($query_text);
         return $query->getResult();
     }
@@ -38,9 +41,7 @@ class Map_model extends Model
     }
 
     public function search($req) {
-        $session = \Config\Services::session();
-        $session->get('ses_data');
-        $query_text = "SELECT a20ux1.DiscoveryTable.discoveryId, a20ux1.DiscoveryTable.takenDate, a20ux1.DiscoveryTable.Longitude, a20ux1.DiscoveryTable.Latitude, a20ux1.DiscoveryTable.title, a20ux1.DiscoveryTable.location, a20ux1.UserTable.userName, a20ux1.DiscoveryPhotosTable.PhotoPath FROM a20ux1.DiscoveryTable INNER JOIN a20ux1.UserTable ON a20ux1.DiscoveryTable.userIdFk = a20ux1.UserTable.userId INNER JOIN a20ux1.DiscoveryPhotosTable ON a20ux1.DiscoveryTable.discoveryId = a20ux1.DiscoveryPhotosTable.discoveryIdFk WHERE a20ux1.DiscoveryPhotosTable.PhotoOrder=1 AND (instr(a20ux1.DiscoveryTable.takenDate, '{$req}') > 0 OR instr(a20ux1.DiscoveryTable.title, '{$req}') > 0 OR instr(a20ux1.DiscoveryTable.description, '{$req}') > 0 OR instr(a20ux1.UserTable.userName, '{$req}') > 0 OR instr(a20ux1.DiscoveryTable.location, '{$req}') > 0);";
+        $query_text = "SELECT a20ux1.DiscoveryTable.discoveryId, a20ux1.DiscoveryTable.takenDate, a20ux1.DiscoveryTable.Longitude, a20ux1.DiscoveryTable.Latitude, a20ux1.DiscoveryTable.title, a20ux1.DiscoveryTable.location, a20ux1.UserTable.userName, a20ux1.DiscoveryPhotosTable.PhotoPath FROM a20ux1.DiscoveryTable INNER JOIN a20ux1.UserTable ON a20ux1.DiscoveryTable.userIdFk = a20ux1.UserTable.userId AND a20ux1.UserTable.useLocation=1 INNER JOIN a20ux1.DiscoveryPhotosTable ON a20ux1.DiscoveryTable.discoveryId = a20ux1.DiscoveryPhotosTable.discoveryIdFk WHERE a20ux1.DiscoveryPhotosTable.PhotoOrder=1 AND (instr(a20ux1.DiscoveryTable.takenDate, '{$req}') > 0 OR instr(a20ux1.DiscoveryTable.title, '{$req}') > 0 OR instr(a20ux1.DiscoveryTable.description, '{$req}') > 0 OR instr(a20ux1.UserTable.userName, '{$req}') > 0 OR instr(a20ux1.DiscoveryTable.location, '{$req}') > 0);";
         $query = $this->db->query($query_text);
         return $query->getResult();
     }
