e4692bb89c6a69f907b9248e0034d2367feaaa70
helena.majoor <helena.majoor@student.kuleuven.be>
2020-11-28 09:00:48 +0100

    Registration works

--- app/Models/User_model.php
+++ app/Models/User_model.php
@@ -32,5 +32,48 @@ class User_model extends Model{
         }
         return $data;
     }
+    
+    
+
+    public function check_email($data){
+        $session = \Config\Services::session();
+        $session->get('ses_data');
+        $query_text = "SELECT a20ux1.UserTable.emailAddress FROM a20ux1.UserTable WHERE a20ux1.UserTable.emailAddress = '{$data}';";
+        $query = $this->db->query($query_text);
+        return $query->getResult();
+
+    }
+
+    public function saveDaysToDb($data){
+        $session = \Config\Services::session();
+        $session->get('ses_data');
+        $query_text = "INSERT INTO a20ux1.ReminderTable (userIdFk, day) VALUES ('{$data['userId']}', '{$data['day']}');";
+        $query = $this->db->query($query_text);
+        return $query->getResult();
+    }
+
+    public function saveToDb($data){
+        $session = \Config\Services::session();
+        $session->get('ses_data');
+        $query_text = "INSERT INTO a20ux1.UserTable (userName, emailAddress, passHash, useLocation) VALUES ('{$data['userName']}', '{$data['emailAddress']}', '{$data['password']}', {$data['location']});";
+        $this->db->query($query_text);
+    }
+
+    public function getUserId($data){
+        $session = \Config\Services::session();
+        $session->get('ses_data');
+        $query_text = "SELECT a20ux1.UserTable.userId FROM a20ux1.UserTable WHERE a20ux1.UserTable.emailAddress = '{$data['emailAddress']}';";
+        $query = $this->db->query($query_text);
+        return $query->getResult();
+    }
+
+    public function login($data) {
+        $session = \Config\Services::session();
+        $session->get('ses_data');
+        $query_text = "SELECT a20ux1.UserTable.emailAddress FROM a20ux1.UserTable WHERE a20ux1.UserTable.emailAddress = '{$data['email']}' AND a20ux1.UserTable.passHash = '{$data['password']}';";
+        $query = $this->db->query($query_text);
+        //echo json_encode($query->getResult());
+        echo $data['email'];
+    }
 
 }
