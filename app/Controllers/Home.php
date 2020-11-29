818bf6c034b51d5e3a75daf7fa7497effa440c30
julianacristina.bertobuzanello <julianacristina.bertobuzanello@student.kuleuven.be>
2020-11-29 15:52:59 +0100

    profile merged v3

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -15,31 +15,10 @@ class Home extends \CodeIgniter\Controller
      * Home constructor.
      */
     public function __construct() {
-        $this->menu_model = new \Menu_model();
         $this->database_model = new Discovery_model();
     }
 
     public function home() {
-        $this->data['menu_items'] = $this->menu_model->get_menuitems('Home');
-        return view('index.php');
-    }
-
-    public function about() {
-        $items = $this->database_model->get_data();
-        $data2['items'] = $items;
-        $this->data['content'] = view('list', $data2);
-
-        $this->data['menu_items'] = $this->menu_model->get_menuitems('about');
-        return view ('about', $this->data);
-    }
-
-    public function contact() {
-        $this->data['menu_items'] = $this->menu_model->get_menuitems('contact');
-        return view('contact', $this->data);
-    }
-
-    public function vue() {
-        $this->data['menu_items'] = $this->menu_model->get_menuitems('vue');
-        return view('vue.html', $this->data);
+        return view('index');
     }
 }
--- app/Views/index.php
+++ app/Views/index.php
@@ -1,1 +1,26 @@
-<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon.ico"><title>frontend</title><link href="https://fonts.googleapis.com/css2?family=Lato&family=Merriweather&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css"><link href="/public/css/app.d6ea8654.css" rel="preload" as="style"><link href="/public/css/chunk-vendors.a3dbb652.css" rel="preload" as="style"><link href="/public/js/app.b164e674.js" rel="preload" as="script"><link href="/public/js/chunk-vendors.62672c63.js" rel="preload" as="script"><link href="/public/css/chunk-vendors.a3dbb652.css" rel="stylesheet"><link href="/public/css/app.d6ea8654.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but frontend doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div><script src="/public/js/chunk-vendors.62672c63.js"></script><script src="/public/js/app.b164e674.js"></script></body></html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/favicon.ico">
+    <title>frontend</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&family=Merriweather&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">
+    <link href="/public/css/app.d6ea8654.css" rel="preload" as="style">
+    <link href="/public/css/chunk-vendors.a3dbb652.css" rel="preload" as="style">
+    <link href="/public/js/app.b164e674.js" rel="preload" as="script">
+    <link href="/public/js/chunk-vendors.62672c63.js" rel="preload" as="script">
+    <link href="/public/css/chunk-vendors.a3dbb652.css" rel="stylesheet">
+    <link href="/public/css/app.d6ea8654.css" rel="stylesheet">
+</head>
+
+<body><noscript><strong>We're sorry but frontend doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
+    <div id="app"></div>
+    <script src="/public/js/chunk-vendors.62672c63.js"></script>
+    <script src="/public/js/app.b164e674.js"></script>
+</body>
+
+</html>
